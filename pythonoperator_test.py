3b25800428025feb83765c8a79591341ac72dfea
Mozartto <1220883974@qq.com>
2024-12-22 20:04:48 +0800

    modify retry_delay

--- pythonoperator_test.py
+++ pythonoperator_test.py
@@ -7,9 +7,9 @@ from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner' : 'tianqi',
-    'start_date':datetime(2021,10,1),
+    'start_date':datetime(2024,12,22),
     'retries':1,
-    'retry_delay':timedelta(minutes=5)
+    'retry_delay':timedelta(minutes=3)
 }
 
 dag = DAG(
