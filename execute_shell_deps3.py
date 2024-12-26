92b09fd4ee497e9382281b5ee9cd64ca3fe6ae59
Mozartto <1220883974@qq.com>
2024-12-26 12:08:37 +0800

    modify owner & start time

--- execute_shell_deps3.py
+++ execute_shell_deps3.py
@@ -5,8 +5,8 @@ from airflow.operators.bash import BashOperator
 
 #实例化DAG
 default_args = {
-    'owner':'zhangsan',# 当前DAG 拥有者
-    'start_date':datetime(2021,10,1), #第一次执行DAG 时间
+    'owner':'rikka',# 当前DAG 拥有者
+    'start_date':datetime(2024,12,26), #第一次执行DAG 时间
     'retries':1,#任务执行失败，重试次数
     'retry_delay':timedelta(minutes=5) # 执行失败重试间隔
 }
