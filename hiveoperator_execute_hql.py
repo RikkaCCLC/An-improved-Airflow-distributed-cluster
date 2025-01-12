0a1092a24e0ee9ab807804d3d2b143e95c9173b3
Mozartto <1220883974@qq.com>
2025-01-12 12:17:58 +0800

    1.12

--- hiveoperator_execute_hql.py
+++ hiveoperator_execute_hql.py
@@ -7,7 +7,7 @@ from airflow.providers.apache.hive.operators.hive import HiveOperator
 
 default_args = {
     'owner' : 'maliu',
-    'start_date':datetime(2025,1,8),
+    'start_date':datetime(2025,1,12),
     'retries':1,
     'retry_delay':timedelta(minutes=3)
 }
