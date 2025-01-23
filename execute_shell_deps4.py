95d327cb2c525f59aaa2edf96d80dc756f5d6c38
Mozartto <1220883974@qq.com>
2025-01-23 22:12:33 +0800

    1.23

--- execute_shell_deps4.py
+++ execute_shell_deps4.py
@@ -8,7 +8,7 @@ default_args = {
     'owner':'zhangsan',# 当前DAG 拥有者
     'start_date':datetime(2025,1,17), #第一次执行DAG 时间
     'retries':1,#任务执行失败，重试次数
-    'retry_delay':timedelta(minutes=5) # 执行失败重试间隔
+    'retry_delay':timedelta(minutes=7) # 执行失败重试间隔
 }
 
 dag = DAG(
