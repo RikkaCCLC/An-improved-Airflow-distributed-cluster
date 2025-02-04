c97e1764f792ad34e37498b060fa37b71b0dca07
Mozartto <1220883974@qq.com>
2025-02-05 00:29:11 +0800

    2.5

--- execute_shell_deps3.py
+++ execute_shell_deps3.py
@@ -8,13 +8,13 @@ default_args = {
     'owner':'rikka',# 当前DAG 拥有者
     'start_date':datetime(2024,12,26), #第一次执行DAG 时间
     'retries':1,#任务执行失败，重试次数
-    'retry_delay':timedelta(minutes=7) # 执行失败重试间隔
+    'retry_delay':timedelta(minutes=3) # 执行失败重试间隔
 }
 
 dag = DAG(
     dag_id='airflow_depends3',
     default_args=default_args,
-    schedule_interval=timedelta(minutes=1),
+    schedule_interval=timedelta(minutes=3),
 )
 
 #定义task
